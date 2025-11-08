75b1656b2d00959e7ae21c905e25340cd15133df
Salih Seferović <salihseferovic05@gmail.com>
2025-11-08 16:03:32 +0100

    enPassant

--- Move.cpp
+++ Move.cpp
@@ -10,9 +10,9 @@ namespace chess {
 	Move::Move()
 		: _from(chess::Position(0,0)) , _to(chess::Position(0, 0))
 	{}
-	Move::Move(chess::Position from, chess::Position to):_from(from),_to(to){}
-	Move::Move(int fromRow, int fromCol, int toRow, int toCol) :_from(chess::Position(fromRow, fromCol)), _to(chess::Position(toRow, toCol)) {}
-	Move::Move(std::string fromSquare, std::string toSquare) {
+	Move::Move(chess::Position from, chess::Position to, bool enPassant):_from(from),_to(to),_enPassant(enPassant){}
+	Move::Move(int fromRow, int fromCol, int toRow, int toCol, bool enPassant) :_from(chess::Position(fromRow, fromCol)), _to(chess::Position(toRow, toCol)), _enPassant(enPassant) {}
+	Move::Move(std::string fromSquare, std::string toSquare, bool enPassant): _enPassant(enPassant) {
 		_from = Position::indexToPos(fromSquare);
 		_to = Position::indexToPos(toSquare);
 
@@ -25,5 +25,8 @@ namespace chess {
 	}
 	chess::Position Move::getToPos()const {
 		return _to;
+	}
+	bool Move::isEnPassant()const {
+		return _enPassant;
 	}
 }
--- Move.h
+++ Move.h
@@ -1,21 +1,23 @@
 #pragma once
 #include<iostream>
 #include<string>
-#include "Position.h";
+#include "Position.h"
 #include<vector>
 namespace chess {
 
 	class Move {
 	private:
 		chess::Position _from, _to;
+		bool _enPassant;
 	public:
 		Move(); // default constructor
 
-		Move(chess::Position from, chess::Position to);
-		Move(int fromRow, int fromCol, int toRow, int toCol);
-		Move(std::string fromSquare, std::string toSquare);
+		Move(chess::Position from, chess::Position to, bool enPassant=false);
+		Move(int fromRow, int fromCol, int toRow, int toCol, bool enpassant = false);
+		Move(std::string fromSquare, std::string toSquare, bool enPassant = false);
 		std::string getMoveText() const;
 		chess::Position getFromPos() const;
 		chess::Position getToPos() const;
+		bool isEnPassant() const;
 	};
 }
